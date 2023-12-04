8bf2a1141b75db00f556ca323e7b902349f50f3e
Mateo ALBER LATOUR <m.alberlatour@gmail.com>
2023-12-04 14:06:35 +0100

    add character class

--- Character.cpp
+++ Character.cpp
@@ -1,1 +1,61 @@
 #include "Character.h"
+
+int Character::getHp()
+{
+	return hp;
+}
+
+int Character::setHp(int health)
+{
+	if (health <= 0) std::cout << "Mort ou vie <= à 0" << std::endl;	
+	else hp = health;
+		
+}
+
+int Character::getAttack()
+{
+	return attack;
+}
+
+int Character::setAttach(int a)
+{
+	if (a < 0) std::cout << "attaque negative" << std::endl;
+	else attack = a;
+	
+}
+
+EDomainExtention Character::getDomain()
+{
+	return domain;
+}
+
+void Character::setDomaine(EDomainExtention e)
+{
+	return;
+}
+
+void Character::setDomaine(int index)
+{
+	return;
+}
+
+int Character::getOccultEnergy()
+{
+	return occultEnergy;
+}
+
+int Character::setOccultEnergy(int oe)
+{
+	if (oe < 0) std::cout << "Energie occulte negative" << std::endl;
+	else occultEnergy = oe;
+}
+
+int Character::getSukunaFingers()
+{
+	return sukunaFingers;
+}
+
+int Character::setSukunaFingers(int sf)
+{
+	return 0;
+}
--- Character.h
+++ Character.h
@@ -3,8 +3,38 @@
 #include <Windows.h>
 #include <mmsystem.h>
 
+enum EDomainExtention
+{
+	InfiniteVoid,
+	CoffinOfTheIronMontain,
+};	
+
 class Character
 {
+private:
+	int hp;
+	int attack;
+	EDomainExtention domain ;
+	int occultEnergy;
+	int sukunaFingers;
+
+
+public:
+	int getHp();
+	int setHp(int health);
+
+	int getAttack();
+	int setAttach(int a);
+
+	EDomainExtention getDomain();
+	void setDomaine(EDomainExtention e);
+	void setDomaine(int index);
+
+	int getOccultEnergy();
+	int setOccultEnergy(int oe);
+
+	int getSukunaFingers();
+	int setSukunaFingers(int sf);
+
 
 };
-
